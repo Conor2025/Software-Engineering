6677cc3bb8ec1c1f3171a97745e77dd923746627
Conor2025 <146126867+Conor2025@users.noreply.github.com>
2024-11-17 12:59:15 -0600

    Create ShowController.java

---
+++ Assignment3/Question 2/ShowController.java
@@ -0,0 +1,20 @@
+package com.example.demo.tvShowController;
+
+import com.example.demo.tvModel.Show;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import java.util.List;
+import java.util.Arrays;
+
+public class ShowController {
+    @GetMapping("/")
+    public String index(Model model) {
+        List<Show> shows = Arrays.asList(
+            new Show("Breaking Bad", "Ozymandias", 10.0),
+            new Show("Attack on Titan", "Hero", 9.9)
+        );
+        model.addAttribute("shows", shows);
+        return "shows"; 
+    }
+    
+}
