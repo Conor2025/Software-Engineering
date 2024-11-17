495789f126bc08533b7db077dec524a854ea3993
Conor2025 <146126867+Conor2025@users.noreply.github.com>
2024-11-17 13:20:04 -0600

    Update Quote.java

--- Assignment3/Question 4/Quote.java
+++ Assignment3/Question 4/Quote.java
@@ -1,1 +1,16 @@
+package com.example.randomquotedisplay.quoteDisplayModel;
 
+public class Quote {
+    private String text;
+    private String author;
+
+    public Quote(String text, String author) {
+        this.text = text;
+        this.author = author;
+    }
+    public String getText() { return this.text; }
+    public void setText(String text) { this.text = text; }
+    public String getAuthor() { return this.author; }
+    public void setAuthor(String author) { this.author = author; }
+
+}
